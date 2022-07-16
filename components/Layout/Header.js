535af7f0d62f85374c0e69bcc066fc8d698b388b
Ikamari <25506025+Ikamari@users.noreply.github.com>
2022-07-16 17:28:36 +0300

    feat: removed example code, added layout and header

---
+++ components/Layout/Header.js
@@ -0,0 +1,7 @@
+import styles from "./styles/Header.module.css"
+
+export default function () {
+    return <div className={styles.header}>
+        <span>Fellow<br/>farmer</span>
+    </div>
+}
---
+++ pages/layout.js
@@ -0,0 +1,8 @@
+import Header from "../components/Layout/Header";
+
+export default function Layout({ children }) {
+    return <>
+        <Header/>
+        <main>{children}</main>
+    </>
+}
